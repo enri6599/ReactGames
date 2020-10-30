2c3e602d08ef8ff94c9f4c0fc30a8dfb92ee2bbb
EnricoZ <mauroenrico99@hotmail.it>
2020-10-30 16:18:14 +0100

    aggiunta alternanza turni e inserimento cellette

--- src/component/Tavolo.js
+++ src/component/Tavolo.js
@@ -1,4 +1,3 @@
-import { Component } from 'react';
 import { Row, Col } from 'reactstrap';
 
 
@@ -7,26 +6,27 @@ function Tavolo(props){
 	for(let i=0;i<7;i++){
 		tavolo.push(
 			<Col>
-				<Colonna onClick={()=>props.onclick(i)}/>
+				<Colonna onClick={()=>props.onClick(i)} valori={props.griglia[i]}/>
 			</Col>
 		);
 	}
 	return <Row className="flex-nowrap no-gutters">{tavolo}</Row>;
 }
 
-function Colonna(){
+function Colonna(props){
 	let colonna=[];
-			for(let i=0;i<6;i++){
+			for(let i=5;i>=0;i--){
 				colonna.push(
-					<Row><Cella/></Row>
+					<Row><Cella onClick={props.onClick} valore={props.valori[i]}/></Row>
 				);
 			}
 	return colonna;
 }
 
 function Cella(props) {
+
 	return (
-		<Col><button className="square"></button></Col>
+	<Col><button onClick={props.onClick} className="square">{props.valore}</button></Col>
 	);
 }
 
--- src/component/tavolaGioco.js
+++ src/component/tavolaGioco.js
@@ -1,29 +1,53 @@
-import react, {Component} from 'react';
-import {Row} from 'reactstrap'
+import /*react,*/ {Component} from 'react';
+//import {Row} from 'reactstrap'
 import Tavolo from './Tavolo';
 
 class Gioco extends Component{
 	constructor(props){
+		let vettore=[];
 		super(props);
+
+		for (let i=0;i<7;i++){
+			vettore.push([]);
+		}
+
 		this.state={
-			griglia: Array(6).fill(Array(7).null),
+			griglia: vettore,
 			stepNum:0,
 			xIsNext:true
 		};
+
+
+		this.handleClick=this.handleClick.bind(this);
+		console.log(this.state.griglia);
 	}
+	
+	handleClick(i){
+		
+		const griglia=this.state.griglia.slice();
+		//console.log(this.state);
+		if(griglia[i].length>=6)
+			return;
+		griglia[i].push(this.state.xIsNext? "X":"O");
+		
+		this.setState({
+			griglia : griglia,
+			xIsNext: !this.state.xIsNext
+		});
+	}
+
+
 	render(){
 		return(
 			<div>
-				<Tavolo onClick={this.onClick}/>
+				<Tavolo onClick={this.handleClick} griglia={this.state.griglia}/>
 			</div>
 			
 		);
 	}
 
 
-	onClick(i){
-
-	}
+	
 }
 
 export default Gioco;
