1cf12510bbba5c66d32ed67dc77a0aba13602919
enri6599 <67325440+enri6599@users.noreply.github.com>
2020-10-31 13:45:59 +0100

    aggiunta la possibilità di tornare indietro nelle mosse

--- src/component/Gioco.js
+++ src/component/Gioco.js
@@ -5,69 +5,90 @@ import checkWin from './controlloVittoria';
 
 class Gioco extends Component{
 	constructor(props){
-		let vettore=[];
 		super(props);
 
-		for (let i=0;i<7;i++)
-			vettore.push([]);
-
 		this.state={
-			history: [{
-				griglia:vettore, 
+			history : [{
+				griglia : Array(7).fill(""),
 				lastInserted: {riga: null, colonna:null}
 			}],
-			stepNum:0,
-			xTurn:true,/*X = rosso - O = giallo*/
+			stepNum : 0,
+			xTurn	: true,/*X = rosso - O = giallo*/
 		};
 
 
 		this.handleClick=this.handleClick.bind(this);
+		//this.modStep=this.modStep.bind(this);
 	}
 	
+	handleClick(i){
+		const stepNum=this.state.stepNum;
+		const history=this.state.history.slice(0, stepNum + 1);
+		
+		const game=history[history.length-1];
+		const griglia=game.griglia.slice();
+
+		if(griglia[i].length>=6 || (stepNum>6?checkWin(game): false)){
+			return;
+		}
+
+		let lastInserted={riga : griglia[i].length, colonna : i}
+		let colonna=griglia[i].concat(this.state.xTurn? "X":"O");
+
+		let grid= griglia.slice(0,i).concat(colonna, griglia.slice(i+1, griglia.length));
+
+		this.setState({
+			history : history.concat({ griglia:grid, lastInserted:lastInserted}),
+			stepNum : stepNum + 1,
+			xTurn 	: !this.state.xTurn
+		});
+	}
+
+	modStep(num){
+		if(Math.abs(num)!==1) return;
+		this.setState({
+			stepNum : this.state.stepNum+num,
+			xTurn	: !this.state.xTurn
+		});
+	}
 	
 
-
 	render(){
+		const stepNum=this.state.stepNum;
+		const history=this.state.history;
 		let stato;
-		if(this.state.stepNum > 6 && checkWin(this.state.history[this.state.history.length-1]))
+		if(stepNum > 6 && checkWin(history[stepNum]))
 			stato= "Ha vinto il player: "+ (!this.state.xTurn? "Rosso":"Giallo")
-		else if(this.state.stepNum >= 42)
+		else if(stepNum >= 42)
 			stato = "Pareggio";
 		else
-			stato= "Tocca al player: " + (this.state.xTurn? "Rosso":"Giallo");
-
+			stato= stepNum+ ": Tocca al player: " + (this.state.xTurn? "Rosso":"Giallo");
 		
+
+		let btnBackDis=stepNum===0;
+		let btnFrontDis=stepNum===history.length-1
+
+
 		return(
-			<div>
-				<div>{stato}</div>
-				<Tavolo onClick={this.handleClick} griglia={this.state.history[this.state.history.length-1].griglia}/>
+			<div className="game">
+				
+				<Tavolo onClick={this.handleClick} griglia={history[stepNum].griglia}/>
+				<div className="game-info">
+					<div>{"Irene Biancardi"}</div>
+					<div >{stato}</div>
+					<div>
+						<button disabled={btnBackDis} onClick={()=>this.modStep(-1)}>{"<--"}</button>
+						<button disabled={btnFrontDis} onClick={()=>this.modStep(1)}>{"-->"}</button>
+					</div>
+					
+				</div>
 				
 			</div>
 			
 		);
 	}
 
-	handleClick(i){
-		const history=this.state.history.slice();
-
-		const game=history[history.length-1];
-		const griglia=game.griglia.slice();
-
-		if(griglia[i].length>=6 || (this.state.stepNum>6?checkWin(game): false)){
-			return;
-		}
-			
-		let lastInserted={riga:griglia[i].length, colonna:i}
-		griglia[i].push(this.state.xTurn? "X":"O");
-		
-		this.setState({
-			history : history.concat({ griglia:griglia, lastInserted:lastInserted}),
-			stepNum : this.state.stepNum+1,
-			xTurn : !this.state.xTurn,
-		});
-		
-
-	}
+	
 }
 
 
